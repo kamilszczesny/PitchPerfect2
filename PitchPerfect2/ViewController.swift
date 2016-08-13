df4a98db0f8c10caa3213df14dd707613fdc8959
Kamil Szczęsny <szczesny.k@gmail.com>
2016-08-13 17:03:16 +0200

    empty project

---
+++ PitchPerfect2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect2
+//
+//  Created by admin on 13.08.2016.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
